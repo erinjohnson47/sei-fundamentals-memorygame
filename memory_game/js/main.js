1e77c317c5afef55de9c82a70240e41b4cab7044
Erin Johnson <erinjohnson47@gmail.com>
2019-06-02 14:24:53 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+function checkForMatch() {
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    console.log("You found a match!");
 } else {
-  alert("Sorry, try again.");
+  console.log("Sorry, not a match. Try again.");	
 }
-}
+};
+};
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+ cardsInPlay.push(cards[cardID]);
+ };
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
