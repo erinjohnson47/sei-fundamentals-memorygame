fc0bb8fc6150319303e12f69ca10876a63618217
Erin Johnson <erinjohnson47@gmail.com>
2019-06-04 21:29:27 -0700

    created game logic - card objects and images

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,40 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 function checkForMatch() {
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
 } else {
-  console.log("Sorry, not a match. Try again.");	
+  alert("Sorry, not a match. Try again.");	
 }
 };
 };
 function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID]);
- cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+ cardsInPlay.push(cards[cardID].rank);
  };
 
 flipCard(0);
