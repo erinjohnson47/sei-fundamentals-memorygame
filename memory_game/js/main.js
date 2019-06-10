21b2f1f09ac62fbbff5cf53fb92687c45f448bb3
Erin Johnson <erinjohnson47@gmail.com>
2019-06-09 19:51:27 -0700

    created game board, added flip card functionality

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,23 +20,33 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
+
+function createBoard() { 
+ 	for (i = 0; i < cards.length; i++) {
+        cards[i];
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+ 	cardElement.setAttribute('data-id', i);
+ 	cardElement.addEventListener('click', flipCard);
+ 	document.getElementById('game-board').appendChild(cardElement);
+ }
+};
+
 var cardsInPlay = [];
+
+function flipCard() { 
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage); 
+ cardsInPlay.push(cards[cardId].rank);
+ if (cardsInPlay.length === 2) {checkForMatch()}
+};
+
 function checkForMatch() {
-if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
 } else {
   alert("Sorry, not a match. Try again.");	
 }
 };
-};
-function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID].rank);
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
- cardsInPlay.push(cards[cardID].rank);
- };
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+ createBoard();
